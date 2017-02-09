756e946d93d3afc7274bd42e0f095bc4b5a72308
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2017-02-09 07:59:57 -0600

    try with cheerio

--- src/TryGetData.jsx
+++ src/TryGetData.jsx
@@ -2,33 +2,69 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
+
 class FetchAnimalData extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      posts: []
+      title: "",
+      release: "",
+      ratings: ""
+
     };
   }
   componentDidMount() {
-    axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
+    axios.get(`http://www.imdb.com/title/tt1229340/`)
     .then(res => {
-      const posts = res.data.data.children.map(obj => obj.data);
-      this.setState({ posts });
-    });
+      let $ = cheerio.load(res);
+      this.setState({
+        allData: $
+      })
+    })
   }
 
   render() {
     return (
       <div>
-      <h1>{`/r/${this.props.subreddit}`}</h1>
-      <ul>
-      {this.state.posts.map(post =>
-        <li key={post.id}>{post.title}</li>
-        )}
-      </ul>
+      <div>{this.state.allData}</div>
       </div>
       );
   }
 }
 
+
 export default FetchAnimalData;
+
+
+
+
+// class FetchAnimalData extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       posts: []
+//     };
+//   }
+//   componentDidMount() {
+//     axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
+//     .then(res => {
+//       const posts = res.data.data.children.map(obj => obj.data);
+//       this.setState({ posts });
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//       <h1>{`/r/${this.props.subreddit}`}</h1>
+//       <ul>
+//       {this.state.posts.map(post =>
+//         <li key={post.id}>{post.title}</li>
+//         )}
+//       </ul>
+//       </div>
+//       );
+//   }
+// }
+
+// export default FetchAnimalData;
