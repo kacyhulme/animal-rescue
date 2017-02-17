1bbef583f7ed9c2128f66957531996421f547e3b
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2017-02-17 07:55:42 -0600

    got data from petfinder

--- src/GetData.jsx
+++ src/GetData.jsx
@@ -8,14 +8,15 @@ class FetchAnimalData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      dogs: []
     };
   }
   componentDidMount() {
     fetchJsonp('http://api.petfinder.com/pet.getRandom?key=188e82af8c0e73cb8c812bc704fed454&type=dog&location=78703&output=full&format=json')
       .then(res => {
-        const posts = res.data.data.children.map(obj => obj.data);
-        this.setState({ posts });
+        return res.json();
+      }).then(function(j) {
+        console.log(j);
       });
   }
 
@@ -24,8 +25,8 @@ class FetchAnimalData extends Component {
       <div>
         <h1>{`/r/${this.props.subreddit}`}</h1>
         <ul>
-          {this.state.posts.map(post =>
-            <li key={post.id}>{post.title}</li>
+          {this.state.dogs.map(dog =>
+            <li key={dog.id}>{dog.title}</li>
           )}
         </ul>
       </div>
