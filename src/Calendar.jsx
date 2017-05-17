cadaa399c885a90818cc5bfbf01909b20f41eed3
Hulme <kacy.hulme@gmail.com>
2017-05-17 16:04:22 -0500

    added initial days for calendar

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -5,3 +5,60 @@ export default React.createClass({
     return <div>Calendar</div>
   }
 })
+import React from 'react'
+
+class CalendarGrid extends React.Component {
+
+  render() {
+    return (
+      <div className="calendar-grid">
+      <CalendarDays numOfDaysInMonth={30}  />
+      </div>
+      )
+  }
+}
+
+class CalendarDays extends React.Component {
+  render() {
+    const days = this.props.numOfDaysInMonth;
+    const arrayOfDays = []
+    let i = 1;
+    for (i = 1; i < days; i ++) {
+      arrayOfDays.push(i)
+    }
+console.log(arrayOfDays)
+
+    return(
+      <div className="calendar-day">
+
+{ arrayOfDays.map((day) =>
+  <div>{day}</div>
+  )
+}
+
+
+      </div>
+      )
+  }
+}
+
+export default CalendarGrid;
+
+
+
+
+// class CalendarDays extends React.Component {
+//   render() {
+//     const days = this.props.numOfDaysInMonth;
+
+//     let i = 0;
+
+//     for (i = 0; i < days; i++) {
+//       console.log(i)
+//     }
+
+//     return(
+//       <div className="calendar-day">{days}</div>
+//       )
+//   }
+// }
